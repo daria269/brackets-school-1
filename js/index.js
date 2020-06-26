7fb1b58d6c1f508a7b807027d3e4bd00f2cb5175
mariaYunB <maria.yun.b@gmail.com>
2020-06-26 17:06:41 +0300

    сделали слайдер

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 const toggleBlock = document.querySelector('.toggle');
 const TOGGLE_ITEM = 'toggle__item';
+const feedbackToggleBlock = document.querySelector('.users-switch');
+const FEEDBACK_TOGGLE_ITEM = 'users-switch__btn';
 
 function chooseSlide(number) {
     showSlides(number)
@@ -26,3 +28,38 @@ toggleBlock.onclick = function(e) {
 }
 
 showSlides(1);
+
+function chooseFeedbackSlide(number) {
+    showFeedbackSlide(number);
+}
+
+function showFeedbackSlide (number) {
+    const slides = document.querySelectorAll('.feedback-card');
+    const toggles = document.querySelectorAll('.users-switch__btn');
+    const triangle = document.createElement('span');
+    triangle.classList.add('users-switch__triangle');
+    const triangleClass = document.querySelector('.users-switch__triangle');
+    toggles[number-1].children[0].innerHTML='';
+
+    triangleClass.parentElement.removeChild(triangleClass);
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.remove('feedback-card--active');
+        toggles[i].classList.remove('users-switch__btn--active');
+    }
+
+    
+
+    slides[number-1].classList.add('feedback-card--active');
+    toggles[number-1].classList.add('users-switch__btn--active');
+    toggles[number-1].append(triangle);
+}
+
+showFeedbackSlide(1);
+
+feedbackToggleBlock.onclick = function(e) {
+    const target = e.target;
+    if (target.parentNode.className === FEEDBACK_TOGGLE_ITEM) {
+        chooseFeedbackSlide(target.parentNode.dataset.userNumber)
+    }
+}
