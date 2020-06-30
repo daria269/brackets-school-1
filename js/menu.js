572af4d4367a122ce9b9aef5d25d055271b824eb
Daria <dasha_kravchenko_00@mail.ru>
2020-06-30 11:11:55 +0300

    переименовали файл js, перенесли строку согласно ревью

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,7 @@
 const menuBtn = document.querySelector('.menu-btn');
+const nav = document.querySelector('.nav');
 
 menuBtn.onclick = function () {
-    const nav = document.querySelector('.nav');
     if(menuBtn.getAttribute('aria-expanded') === 'true') {
         menuBtn.setAttribute('aria-expanded', 'false');
         nav.classList.remove('nav--active');
