16da116d6a8b891b204c55599eb7a15769e9a631
JohnC27 <evgeniy_evgeniy_89@gmail.com>
2020-07-03 21:00:36 +0500

    сделали правки по ревью

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,7 @@
 const menuBtn = document.querySelector('.menu-btn');
 const nav = document.querySelector('.nav');
 
-menuBtn.onclick = function () {
-    if(menuBtn.getAttribute('aria-expanded') === 'true') {
-        menuBtn.setAttribute('aria-expanded', 'false');
-        nav.classList.remove('nav--active');
-    } else {
-        menuBtn.setAttribute('aria-expanded', 'true');
-        nav.classList.add('nav--active');
-    }
-}
+menuBtn.addEventListener('click', function () {
+    menuBtn.setAttribute('aria-expanded', menuBtn.getAttribute('aria-expanded') === "true" ? "false" : "true");
+    nav.classList.toggle('nav--active');
+});
