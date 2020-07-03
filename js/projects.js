ec92ca72316a2f5983b631a0c89a6c499ddbb223
JohnC27 <evgeniy_evgeniy_89@gmail.com>
2020-07-04 00:30:40 +0500

    зарезолвили, починили слайдеры

--- js/projects.js
+++ js/projects.js
@@ -1,28 +1,23 @@
 const toggleBlock = document.querySelector('.toggle');
-const TOGGLE_ITEM = 'toggle__item';
+const projectsToggles = document.querySelectorAll('.toggle__item');
 
-function chooseSlide(number) {
-    showSlides(number)
-}
-
+//слайдер для учебных проектов
 function showSlides(number) {
     const slides = document.querySelectorAll('.card');
-    const dots = document.querySelectorAll('.toggle__item');
 
     for (let i = 0; i < slides.length; i++) {
         slides[i].classList.remove('card--active');
-        dots[i].classList.remove('toggle__item--active');
+        projectsToggles[i].classList.remove('toggle__item--active');
     }
 
-    slides[number-1].classList.add('card--active');
-    dots[number-1].classList.add('toggle__item--active');
+    slides[number - 1].classList.add('card--active');
+    projectsToggles[number - 1].classList.add('toggle__item--active');
 }
 
-toggleBlock.onclick = function(e) {
-    const target = e.target;
-    if (target.className === TOGGLE_ITEM) {
-        chooseSlide(target.dataset.number)
-    }
+for(let i = 0; i < projectsToggles.length; i++) {
+    projectsToggles[i].addEventListener('click', function() {
+        showSlides(i + 1);
+    })
 }
 
-showSlides(1);
+showSlides(defaultSlideNumber);
